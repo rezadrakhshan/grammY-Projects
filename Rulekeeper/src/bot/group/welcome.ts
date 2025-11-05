fb2d5abf8ee68d7e6bbb99630131516f48b220f9
Derakhshan <github2reza@gmail.com>
2025-11-05 17:07:49 +0330

    feat(welcome_message): handle new_chat_members event for send welcome message

--- Rulekeeper/src/bot/group/welcome.ts
+++ Rulekeeper/src/bot/group/welcome.ts
@@ -15,3 +15,15 @@ welcome.command("set_welcome", async (ctx) => {
   if (!result) await ctx.reply(ctx.t("welcome_unsuccessful"));
   else await ctx.reply(ctx.t("welcome_successful"));
 });
+
+welcome.on("message:new_chat_members", async (ctx) => {
+  const members = ctx.message.new_chat_members;
+  const group = await Group.findOne({ chatID: ctx.chat.id });
+  if (!group) return;
+  if (group.welcomeMessage !== "") {
+    for (const member of members) {
+      if (member.is_bot) return;
+      await ctx.reply(`@${member.username} ${group.welcomeMessage}`);
+    }
+  }
+});
