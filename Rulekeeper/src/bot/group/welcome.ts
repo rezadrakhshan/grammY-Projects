4aecfd505ff1af97f119e8af393c21c6985970b7
Derakhshan <github2reza@gmail.com>
2025-11-07 15:53:31 +0330

    feat(user): Developed handler for add users in database amd handle left member event

--- Rulekeeper/src/bot/group/welcome.ts
+++ Rulekeeper/src/bot/group/welcome.ts
@@ -1,6 +1,7 @@
 import { Composer } from "grammy";
 import { type MyContext } from "../../index.js";
 import { Group } from "../../database/models/group.js";
+import { User } from "../../database/models/user.js";
 
 export const welcome = new Composer<MyContext>();
 
@@ -23,7 +24,31 @@ welcome.on("message:new_chat_members", async (ctx) => {
   if (group.welcomeMessage !== "") {
     for (const member of members) {
       if (member.is_bot) return;
+      const check = await User.findOne({ userID: member.id });
+      if (!check)
+        await User.create({
+          userID: member.id,
+          username: member.username,
+          firstName: member.first_name,
+          groups: [group.chatID],
+        });
+      else {
+        await User.findOneAndUpdate(
+          { userID: member.id },
+          { $push: { groups: group.chatID } },
+        );
+      }
       await ctx.reply(`@${member.username} ${group.welcomeMessage}`);
     }
+  }
+});
+
+welcome.on("message:left_chat_member", async (ctx) => {
+  const member = ctx.message.left_chat_member;
+  const chat = ctx.chat.id;
+  let user = await User.findOne({ userID: member.id });
+  if (user) {
+    user.groups = user.groups.filter((item) => item !== chat);
+    await user.save();
   }
 });
