703078759e179c560eb576c1e7fa17f576ea0497
Derakhshan <github2reza@gmail.com>
2025-11-28 12:50:29 +0330

    fix(admin guard): fix bug in isAdmin guard

--- Rulekeeper/src/guards/admin.ts
+++ Rulekeeper/src/guards/admin.ts
@@ -3,25 +3,15 @@ import { type NextFunction } from "grammy";
 
 export async function isAdmin(ctx: MyContext, next: NextFunction) {
   try {
-    if (ctx.message && ctx.message.entities) {
-      const commandEntity = ctx.message.entities.find(
-        (entity) => entity.type === "bot_command",
-      );
-      if (commandEntity) {
-        if (ctx.chat && ctx.chat.type !== "private") {
-          const member = await ctx.getChatMember(ctx.from?.id as number);
-          if (
-            member.status !== "administrator" &&
-            member.status !== "creator"
-          ) {
-            await ctx.reply(ctx.t("only_admin"));
-            return;
-          }
-        } else {
-          await ctx.reply("This command can only be used in a group chat.");
-          return;
-        }
+    if (ctx.chat && ctx.chat.type !== "private") {
+      const member = await ctx.getChatMember(ctx.from?.id as number);
+      if (member.status !== "administrator" && member.status !== "creator") {
+        await ctx.reply(ctx.t("only_admin"));
+        return;
       }
+    } else {
+      await ctx.reply("This command can only be used in a group chat.");
+      return;
     }
     await next();
   } catch (err) {
